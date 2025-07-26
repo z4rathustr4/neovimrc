86ee4492602d6bf0173ae5655ce9c3f9d7ae27f2
Friedrich Nietzsche <ringzer0dev@gmail.com>
2025-07-26 11:01:08 +0100

    fix: lualine complaining about theme

--- nvim/lua/z4rathustr4/plugins/lualine.lua
+++ nvim/lua/z4rathustr4/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
     opts = {
         options = {
             icons_enabled = false,
-            theme = 'gruber-darker',
+            theme = 'auto',
             component_separators = '|',
             section_separators = '',
         },
