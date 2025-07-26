b80462af6dcdb18874bb5a76f8f8b1a9ffe1c3d6
Friedrich Nietzsche <ringzer0dev@gmail.com>
2025-07-26 14:02:32 +0100

    modified: nvim

--- nvim/lua/z4rathustr4/set.lua
+++ nvim/lua/z4rathustr4/set.lua
@@ -20,6 +20,9 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
@@ -28,21 +31,15 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- vim.keymap.set("n", "<leader>vwm", function()
---   require("vim-with-me").StartVimWithMe()
--- end)
--- vim.keymap.set("n", "<leader>svwm", function()
---   require("vim-with-me").StopVimWithMe()
--- end)
 
 -- greatest remap ever
-
 vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>gb", "<cmd>!go build .<CR>")
 vim.keymap.set("n", "<leader>gr", "<cmd>!go run .<CR>")
+vim.keymap.set("n", "<leader>mm", "<cmd>!cargo build <CR>")
+
 
 -- next greatest remap ever : asbjornHaland
-
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
@@ -51,7 +48,7 @@ vim.keymap.set("v", "<leader>d", "\"_d")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", function()
-  vim.lsp.buf.format()
+	vim.lsp.buf.format()
 end)
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
@@ -62,11 +59,11 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-j>", "<C-w>j")
 vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<A-h>", "<cmd>ToggleTerm size=15 direction=horizontal<CR>")
-vim.api.nvim_set_keymap('t', '<A-h>', '<cmd>ToggleTerm<CR>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<A-h>', '<cmd>ToggleTerm<CR>', { noremap = true, silent = true })
 
 vim.g.mapleader = " "
 vim.diagnostic.config({
-  virtual_text = false,
+	virtual_text = false,
 })
 vim.o.guicursor = "n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
 vim.g.python_host_prog = "/usr/bin/python"
